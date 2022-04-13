69d42e3ec5b65f008e0647a73ff02609edd03d8a
floschne <florian.schneider-1@uni-hamburg.de>
2022-04-13 14:30:56 +0200

    using spacy model wheels directly instead of tars

--- src/celery_worker_entrypoint.sh
+++ src/celery_worker_entrypoint.sh
@@ -9,22 +9,22 @@ mkdir -p /spacy_models
 #  not grow too large. To avoid downloading the model everytime, it can be mapped into the container with a volume in
 #  the docker-compose
 
-if [ ! -f /spacy_models/de_core_news_lg-3.2.0.tar.gz ]; then
-  wget -P /spacy_models https://github.com/explosion/spacy-models/releases/download/de_core_news_lg-3.2.0/de_core_news_lg-3.2.0.tar.gz
+if [ ! -f /spacy_models/de_core_news_lg-3.2.0-py3-none-any.whl ]; then
+  wget -P /spacy_models https://github.com/explosion/spacy-models/releases/download/de_core_news_lg-3.2.0/de_core_news_lg-3.2.0-py3-none-any.whl
 else
   echo "spaCy model 'de_core_news_lg' already downloaded!"
 fi
 if [ ! -d /usr/local/lib/python3.9/site-packages/de_core_news_lg ]; then
-  pip install /spacy_models/de_core_news_lg-3.2.0.tar.gz
+  pip install /spacy_models/de_core_news_lg-3.2.0-py3-none-any.whl
 fi
 
-if [ ! -f /spacy_models/en_core_web_trf-3.2.0.tar.gz ]; then
-   wget -P /spacy_models https://github.com/explosion/spacy-models/releases/download/en_core_web_trf-3.2.0/en_core_web_trf-3.2.0.tar.gz
+if [ ! -f /spacy_models/en_core_web_trf-3.2.0-py3-none-any.whl ]; then
+   wget -P /spacy_models https://github.com/explosion/spacy-models/releases/download/en_core_web_trf-3.2.0/en_core_web_trf-3.2.0-py3-none-any.whl
 else
   echo "spaCy model 'en_core_web_trf' already downloaded!"
 fi
 if [ ! -d /usr/local/lib/python3.9/site-packages/en_core_web_trf ]; then
-  pip install /spacy_models/en_core_web_trf-3.2.0.tar.gz
+  pip install /spacy_models/en_core_web_trf-3.2.0-py3-none-any.whl
 fi
 
 
