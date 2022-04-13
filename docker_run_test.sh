0d9d22667e828fda9344b9b48f443b81b92379f9
floschne <florian.schneider-1@uni-hamburg.de>
2022-04-13 16:25:10 +0200

    renamed test script

--- docker_run_test.sh
+++ docker_run_test.sh
@@ -2,5 +2,5 @@
 
 docker-compose down -v --remove-orphans
 docker-compose up -d
-docker-compose exec -T dwts_backend bash /dwts_code/src/start_tests.sh "$@"
+docker-compose exec -T dwts_backend bash /dwts_code/src/run_tests.sh "$@"
 docker-compose down -v --remove-orphans
